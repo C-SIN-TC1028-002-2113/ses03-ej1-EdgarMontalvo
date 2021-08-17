269c6302c63014d1daac4d726ab0fa4854dd7f03
EdgarMontalvo <A01742343@itesm.mx>
2021-08-17 14:53:06 +0000

    13 PINTURAS SESION 3

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -2,7 +2,7 @@ import math
 def main():
     #escribe tu código abajo de esta línea
     a = float(input("Area a pintar en metros: "))
-    r = float(input("Rendimiento: "))
+    r = float(input("Rendimiento (m2/l): "))
     l = math.ceil(a/r)
     print("Litros a comprar:",l)
 if __name__ == '__main__':
